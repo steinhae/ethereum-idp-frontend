4c9f78ec3576d0cd71d4d5ae0d642d87798fd36d
Hannes <h.steinhaeuser@gmx.de>
2017-06-15 16:52:54 +0200

    Testnet sync, UI clean up

--- Repairchain/app/src/main/java/de/tum/repairchain/MainActivity.java
+++ Repairchain/app/src/main/java/de/tum/repairchain/MainActivity.java
@@ -1,16 +1,25 @@
 package de.tum.repairchain;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
+import android.view.WindowManager;
 import android.widget.TextView;
 import org.ethereum.geth.*;
 import android.util.Log;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.txt_version) TextView txtVersion;
+    @BindView(R.id.txt_latestblock) TextView txtLatestBlock;
+
     private long lastUpdate = 0;
-    private TextView textbox;
+    private EthereumClient ethereumClient;
+    private Context ctx;
+    private Node node;
 
     NewHeadHandler handler = new NewHeadHandler() {
         @Override public void onError(String error) { }
@@ -20,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.runOnUiThread(new Runnable() {
                     public void run() {
                         String txt = "#" + header.getNumber() + ": " + header.getHash().getHex().substring(0, 10) + "…\n";
-                        textbox.append(txt);
+                        txtLatestBlock.setText(txt);
                         Log.v("NewBlock", txt);
                     }
                 });
@@ -33,37 +42,71 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         setTitle("Android In-Process Node");
-        textbox = (TextView) findViewById(R.id.textView);
-        textbox.setMovementMethod(new ScrollingMovementMethod());
 
-        Context ctx = new Context();
+        if (savedInstanceState == null) {
+            ctx = new Context();
+            if (initTestnetNode()) {
+                NodeInfo info = node.getNodeInfo();
+                txtVersion.setText("My name: " + info.getName() + "\n");
+                //            textbox.append("My address: " + info.getListenerAddress() + "\n");
+                //            textbox.append("My protocols: " + info.getProtocols() + "\n\n");
 
+                if (initEthereumNode()) {
+                    subscribeToNewHead();
+                    displayLatestBlock();
+                } else {
+                    finish();
+                }
+            }
+        } else {
+            Log.d("OnCreate","Node already initialized.");
+        }
+    }
+
+    private boolean initTestnetNode() {
+        NodeConfig nc = new NodeConfig();
+        nc.setEthereumNetworkID(3);
+        nc.setWhisperEnabled(true);
+        nc.setEthereumEnabled(true);
+        String genesis = Geth.testnetGenesis();
+        nc.setEthereumGenesis(genesis);
         try {
-
-            NodeConfig nc = new NodeConfig();
-            nc.setEthereumNetworkID(3);
-            nc.setWhisperEnabled(true);
-            nc.setEthereumEnabled(true);
-            String genesis = Geth.testnetGenesis();
-            nc.setEthereumGenesis(genesis);
-            Node node = Geth.newNode(getFilesDir() + "/.ethereum", nc);
+            node = Geth.newNode(getFilesDir() + "/.ethereum", nc);
             node.start();
-
-            NodeInfo info = node.getNodeInfo();
-            textbox.append("My name: " + info.getName() + "\n");
-            textbox.append("My address: " + info.getListenerAddress() + "\n");
-            textbox.append("My protocols: " + info.getProtocols() + "\n\n");
-
-            EthereumClient ec = node.getEthereumClient();
-            ec.subscribeNewHead(ctx, handler,  16);
-            textbox.append("Latest block: " + ec.getBlockByNumber(ctx, -1).getNumber() + ", syncing...\n");
-
-            lastUpdate = System.currentTimeMillis();
-
+            return true;
         } catch (Exception e) {
-            e.printStackTrace();
+            Log.e("InitTestNet","Init of Testnet node failed: " + e.getMessage());
+            return false;
+        }
+    }
+
+    private boolean initEthereumNode() {
+        try {
+            ethereumClient = node.getEthereumClient();
+        } catch (Exception e) {
+            Log.e("GetEthereumClient", "Failed to get the Ethereum client: " + e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    private boolean subscribeToNewHead() {
+        try {
+            ethereumClient.subscribeNewHead(ctx, handler, 16);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void displayLatestBlock() {
+        try {
+            txtLatestBlock.setText("Latest block: " + ethereumClient.getBlockByNumber(ctx, -1).getNumber() + ", syncing...\n");
+        } catch (Exception e) {
+            Log.d("DisplayLatestBlock", "Could not display latest block: " + e.getMessage());
         }
     }
 }
